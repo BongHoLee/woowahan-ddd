10b41ab520672cde787fb87508c9cbdabcb7eeb9
bongholee <leebongho9204@gmail.com>
2024-09-01 16:09:21 +0900

    Spring-data-jdbc version init

---
+++ ddd-essence-jdbc/src/main/java/org/beaoh/base/AggregateRoot.java
@@ -0,0 +1,32 @@
+package org.beaoh.base;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.AfterDomainEventPublication;
+import org.springframework.data.domain.DomainEvents;
+import org.springframework.util.Assert;
+
+public abstract class AggregateRoot<T extends DomainEntity<T, TID>, TID> extends DomainEntity<T, TID> {
+    @Transient
+    private final transient List<Object> domainEvents = new ArrayList();
+
+    public AggregateRoot() {}
+
+    protected void registerEvent(T event) {
+        Assert.notNull(event, "Domain event must not be null");
+        this.domainEvents.add(event);
+    }
+
+    @AfterDomainEventPublication
+    protected void clearDomainEvents() {
+        this.domainEvents.clear();
+    }
+
+    @DomainEvents
+    protected Collection<Object> domainEvents() {
+        return Collections.unmodifiableList(this.domainEvents);
+    }
+}
